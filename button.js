0145e67386d3268425f7d00f74c37d6e00fa0ff6
Edwin Munene <edwinmunene95@gmail.com>
2026-01-27 14:53:54 +0300

    Update button.js

--- button.js
+++ button.js
@@ -1,1 +1,9 @@
-
+             <script>  
+  (function(d, s, id){
+     var js, mpesa = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://cdn.jsdelivr.net/gh/muaad/mpesa_button@master/src/button.min.js";
+     mpesa.parentNode.insertBefore(js, mpesa);
+   }(document, 'script', 'mpesa_btn_js'));
+</script>
