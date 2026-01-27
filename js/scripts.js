2937e3c6b99b1abcc93f7399986a42c365956d36
Edwin Munene <edwinmunene95@gmail.com>
2026-01-27 14:39:44 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -243,5 +243,15 @@ $(document).ready(function(){
         $(".order").text(newPizza.order())
         $(".ordertotal").text(total);
         $("#myForm").trigger("reset");
+(function(d, s, id){
+     var js, mpesa = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://cdn.jsdelivr.net/gh/muaad/mpesa_button@master/src/button.min.js";
+     mpesa.parentNode.insertBefore(js, mpesa);
+   }(document, 'script', 'mpesa_btn_js'));
+        
+
+        
 
      
